ac37547c40a83357f1a946315e9df29a33a9bfd5
Chaterina Fransisca <chaterinamariaf@gmail.com>
2021-09-16 19:47:18 +0700

    Create the layout for the main feature (Clock In & Clock Out)

--- app/src/main/java/com/example/attendance/SplashScreen.kt
+++ app/src/main/java/com/example/attendance/SplashScreen.kt
@@ -4,10 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.example.attendance.R
 import com.google.android.material.imageview.ShapeableImageView
 
 class SplashScreen : AppCompatActivity() {
-    private lateinit var handler: Handler
     private lateinit var splash: ShapeableImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,12 +17,10 @@ class SplashScreen : AppCompatActivity() {
         splash = findViewById(R.id.splash)
         splash.animate().alpha(4000F).setDuration(0)
 
-        handler.postDelayed(Runnable {
-                fun run(){
-                    val intent = Intent(this, Login::class.java)
-                    startActivity(intent)
-                    finish()
-                }
-        }, 4000)
+        Handler().postDelayed({
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
