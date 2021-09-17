de01d81282e74c110349083eb31f13fbc3c58514
Chaterina Fransisca <chaterinamariaf@gmail.com>
2021-09-18 00:36:15 +0700

    Update the layout for the main feature and add the logout floating button

---
+++ app/src/main/java/com/example/attendance/ui/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.attendance.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.attendance.R
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var logoutFab: ExtendedFloatingActionButton
+    private lateinit var timeShow: TextView
+    private lateinit var scheduleDate: TextView
+    private lateinit var locatTV: TextView
+    private lateinit var clockInBtn: MaterialButton
+    private lateinit var clockOutBtn: MaterialButton
+    private lateinit var timeLog: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        logoutFab = findViewById(R.id.logout_fab)
+        timeShow = findViewById(R.id.timeTV)
+        scheduleDate = findViewById(R.id.scheduleTV)
+        locatTV = findViewById(R.id.locationTV)
+        clockInBtn = findViewById(R.id.clockIn_Btn)
+        clockOutBtn = findViewById(R.id.clockOut_Btn)
+        timeLog = findViewById(R.id.timeLogRV)
+
+        timeShow.setText(getCurrentTime())
+        scheduleDate.setText("${getDay()}, ${getTodayState()}")
+    }
+
+    fun getCurrentTime(): String {
+        val currentTime = SimpleDateFormat("hh:mm a", Locale.getDefault()).format(Date())
+        return currentTime
+    }
+
+    fun getDay():  String{
+        val day = SimpleDateFormat("EEEE", Locale.getDefault()).format(Date())
+        return day
+    }
+    fun getTodayState(): String{
+        val currentDate = SimpleDateFormat("dd/MM/yyyy",Locale.getDefault()).format(Date())
+        return currentDate
+    }
+}
--- app/src/main/java/com/example/attendance/ui/SplashScreen.kt
+++ app/src/main/java/com/example/attendance/ui/SplashScreen.kt
@@ -1,4 +1,4 @@
-package com.example.attendance
+package com.example.attendance.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
